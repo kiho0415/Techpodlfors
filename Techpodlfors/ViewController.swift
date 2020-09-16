e6986358935323c7a2e5929a7c2d30d59218cec6
KIho Mmorita <kiho.h1.chukecheese@gmail.com>
2020-09-16 18:20:05 +0900

    done

--- Techpodlfors/ViewController.swift
+++ Techpodlfors/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate{
+    
+    @IBOutlet var table:UITableView!
+    var songnameArray = [String]()
+    var filenameArray = [String]()
+    var imagenameArray = [String]()
+    var audioPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.dataSource = self
+        table.delegate = self
+        songnameArray = ["カノン","エリーゼのために","G線上のアリア"]
+        filenameArray = ["cannon","elise","aria"]
+        imagenameArray = ["Pachelbel.jpg","Beethoven.jpg","Bach.jpg"]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songnameArray.count
     }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = songnameArray[indexPath.row]
+        cell?.imageView?.image = UIImage(named: imagenameArray[indexPath.row])
+        return cell!
+    }
+    //せるが押された時に呼ばれるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(songnameArray[indexPath.row])が選ばれました")
+        let auidioPath = URL(fileURLWithPath: Bundle.main.path(forResource: filenameArray[indexPath.row], ofType: "mp3")!)
+        audioPlayer = try? AVAudioPlayer(contentsOf: auidioPath)
+        audioPlayer.play()
+    }
 }
 
